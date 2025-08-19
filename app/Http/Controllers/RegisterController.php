a869b7e959d1f575edd098844d4376f4ef4d6de2
Hadi Razal <hadhirasal22@gmail.com>
2025-08-19 23:15:24 +0400

    register controller added

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController
 {
-    //
+    public function register(Request $request){
+
+        $request->validate([
+            'email'=>['required','email'],
+            'password'=>['required','min:8'],
+            'Name'=>['required','min:3'],
+        ]);
+
+        $user = User::create([
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'name'=>$request->Name,
+        ]);
+
+        return view('login', ['message' => 'Registration successful. Please log in.',"userDetails" => $user ]);
+
+
+
+    }
 }
