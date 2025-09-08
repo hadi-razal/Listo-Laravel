1f9dd9954e9b3c9ca01ad715f3cd2de7f287bcbc
Hadi Razal <Hadhirasal22@gmail.com>
2025-09-08 17:47:30 +0400

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,40 +2,44 @@
 
 
 @section('content')
-    <div class="mx-auto flex h-[90vh] items-center justify-center p-4 bg-red-50">
+    <div class="mx-auto flex  items-center justify-center p-4 bg-red-50">
 
-        <div class="max-w-7xl flex items-center justify-center flex-col">
+        <div class="max-w-7xl h-[80vh] flex items-center justify-center flex-col">
 
 
-            <h1 class="font-bold text-center text-[70px]">
+            <h1 class="font-bold text-center font-[Poppins] text-[70px]">
                 The Best and Secure notes app for you
             </h1>
 
 
 
 
-            <p class="text-center font-extralight w-max-3xl">
+            <p class="text-center word-break font-extralight max-w-3xl">
 
                 Industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
                 printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
 
-
             </p>
+
+
+
 
             <div class="flex gap-4 items-center justify-center">
 
 
-                <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded cursor-pointer">
+                <button
+                    class="mt-4 bg-blue-500 hover:bg-blue-700  text-white font-light py-2 px-4 rounded-md cursor-pointer">
                     Get Started
                 </button>
 
 
-                <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded cursor-pointer">
+                <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-md cursor-pointer">
                     View Demo
                 </button>
 
 
             </div>
+
 
         </div>
 
