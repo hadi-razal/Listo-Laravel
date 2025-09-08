5610180aeda689a498509185f9551ac2bfab8f0f
Hadi Razal <Hadhirasal22@gmail.com>
2025-09-08 17:27:30 +0400

    created a hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <title>Home - Listo</title>
+@extends('layouts.guest')
 
 
+@section('content')
+
+    <div class="mx-auto flex h-[90vh] items-center justify-center p-4 bg-red-50">
+
+        <div class="max-w-7xl flex items-center justify-center flex-col">
 
 
-</head>
-
-<body className=" bg-gray-100">
-
-    @extends('layout.guest')
+            <h1 class="text-extra-bold text-center text-[70px]">
+                The Best and Secure notes app for you
+            </h1>
 
 
-    <div className="mx-auto flex items-center justify-center h-screen p-4  pt-[100px]">
-
-        <h1 className="">The Best and Secure notes app for you</h1>
-        
 
 
-        <p>ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
-            recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+             <p class="text-center">
+
+                Industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+            </p>
+
+
+            <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+                Get Started
+            </button>
+
+        </div>
+
 
     </div>
 
-
-</body>
-
-</html>
+@endsection
